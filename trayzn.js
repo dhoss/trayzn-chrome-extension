863fe7ce66588198275d966fe21e0a58467c1227
Devin Austin <devin.austin@gmail.com>
2023-08-24 16:23:05 -0600

    correct tab info is being transmitted

--- trayzn.js
+++ trayzn.js
@@ -1,11 +1,18 @@
-chrome.action.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(async function(tab) {
+  let tabInfo = await chrome.tabs.get(tab.id);
+  console.log("Tab info: ", tabInfo);
   fetch("http://localwsl.com:8080/api/v1/bookmarks/add", {
               method: 'POST',
               headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json'
               },
-              body: JSON.stringify({key: "value"})
+              body: JSON.stringify(
+                {
+                  url: tabInfo.url,
+                  title: tabInfo.title,
+                  favIconUrl: tabInfo.favIconUrl
+                })
           })
               .then(response => response.json())
               .then(response => console.log('Response: ', response))
