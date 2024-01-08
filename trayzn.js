dedbfcee3ef4465132d468840ea841737eae4181
Devin Austin <devin.austin@gmail.com>
2024-01-07 17:23:57 -0700

    notes to get popup working

--- trayzn.js
+++ trayzn.js
@@ -1,3 +1,7 @@
+// this will more than likely need to be rewritten as a content.js script
+// where the default_popup is defined (add_bookmark_result.html) and it has a script which makes the fetch call using
+// the tab information passed in as a message, and then updating the save-bookmark-result div with a result message
+// https://stackoverflow.com/questions/61924508/pass-a-information-between-content-and-popup-on-chrome-extensions
 chrome.action.onClicked.addListener(async function(tab) {
   let tabInfo = await chrome.tabs.get(tab.id);
   let configuration = await chrome.storage.local.get(['url', 'apiKey', 'apiSecret']);
@@ -17,6 +21,8 @@ chrome.action.onClicked.addListener(async function(tab) {
                 })
           })
               .then(response => response.json())
-              .then(response => console.log('Response: ', response))
+              .then(response => {
+                 console.log('Response: ', response);
+               })
               .catch(error => console.log('Error:', error));
 });
